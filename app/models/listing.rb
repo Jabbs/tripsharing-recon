4eb124b0cfdf93ea7640bdece0799eb7b532e537
Pete Jabbour <pete@learnmetrics.com>
2015-04-07 14:01:41 -0500

    changed algorythm

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -40,9 +40,9 @@ class Listing < ActiveRecord::Base
     form.submit
     
     # 10 per page
-    200.times do |n|
+    100.times do |n|
       url_first = "https://www.lonelyplanet.com/thorntree/forums/travel-companions?page="
-      page_number = (n + 28).to_s
+      page_number = (n + 70).to_s
       url = url_first + page_number
       page = agent.get(url)
       
