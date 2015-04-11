e4c318abfd45d8ba108f4fe35eb6c73f4d963a8b
Pete Jabbour <pete@learnmetrics.com>
2015-04-11 12:03:04 -0500

    more

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -83,7 +83,7 @@ class Listing < ActiveRecord::Base
     # 10 per page
     300.times do |n|
       url_first = "https://www.lonelyplanet.com/thorntree/forums/travel-companions?page="
-      page_number = (n + 779).to_s
+      page_number = (n + 797).to_s
       url = url_first + page_number
       page = agent.get(url)
       
